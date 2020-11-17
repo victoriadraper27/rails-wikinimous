86608d5357da01797d5fae518fb9b8ba689bc587
Victoria Draper <victoria.draper27@gmail.com>
2020-11-17 16:14:31 +0900

    Added Faker and Simple Form Gem

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,40 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, except: [:index, :new, :create]
+
+  def index
+    @articles = Aritcle.all
+  end
+
+  def show; end
+
+  def new
+    @article = Aritcle.new
+  end
+
+  def create
+    @article = Aritcle.create(article_params)
+    redirect_to article_path(article)
+  end
+
+  def edit; end
+
+  def update
+    @article.update(article_params)
+    redirect_to article_path(article)
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def find_article
+    @article = Aritcle.find(params[:id])
+  end
+
+  def article_params
+    params.require(article).permit(:title, :content)
+  end
 end
